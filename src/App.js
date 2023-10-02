90bf1234bd9d006a7d95bbf18571659b0c7ff707
RHKarthick <harikarthick619@gmail.com>
2023-10-02 18:08:51 +0530

    grid-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card/Card";
 import HeroSection from "./components/HeroSection/HeroSection";
 //@ts-ignore
 import NavBar from "./components/NavBar/NavBar";
+import { fetchTopAlbums } from "./components/api/api";
 
 function App() {
+  const [topAlbumData, settopAlbumData] = useState([]);
+  const generateTopAlbumData = async () => {
+    const data = await fetchTopAlbums();
+    console.log(data);
+    settopAlbumData(data);
+  };
+  useEffect(() => {
+    generateTopAlbumData();
+  }, []);
+  console.log(topAlbumData, "topAlbumData");
   return (
     <div className="App">
       <NavBar />
       <HeroSection />
-      <Card/>
+      <div className="cardContainer">
+      {topAlbumData.map((item) => {
+        return <Card key={item.id} data={item} type="album" />;
+      })}
+      </div>
     </div>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Styles from "./Card.module.css";
 import { ReactComponent as CardImage } from "../../assets/CardImage.svg";
+import { Chip } from "@mui/material";
 
-const Card = () => {
-  return (
-    <>
-    <div className={Styles.cardContainer}>
-      <CardImage />
-     <div className={Styles.cardFollowersCount} > 100M Follows</div>
-    </div>
-    <div  className={Styles.cardTitle}>New Bollywood</div>
-    </>
-  );
+const Card = ({ data, type }) => {
+  const getCard = (type) => {
+    switch (type) {
+      case "album": {
+        const { image, follows, title, songs } = data;
+
+        return (
+          <div className={Styles.wrapper}>
+            <div className={Styles.card}>
+              <img src={image} alt="album" width={160} />
+              <div className={Styles.banner}>
+                <Chip  className={Styles.chip} label={`${follows}`} Follows  size="small" />
+              </div>
+            </div>
+            <div className={Styles.titleWrapper}>
+              <p>{title}</p>
+            </div>
+          </div>
+        );
+      }
+      default:
+        return <></>;
+    }
+  };
+  return getCard(type);
 };
 
 export default Card;
